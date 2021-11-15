e3e6a81fcdb100dbc73b9f89067c758996bb7bcd
uuunderflow <uuunderflow@yandex.com>
2021-11-15 14:41:47 +0800

    update

--- main.c
+++ main.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 char g_buf[0x100];
 
 void bof1() {
     char buf[0x10];
-    fread(g_buf, sizeof(g_buf), 1, stdin);
+    read(0, g_buf, sizeof(g_buf));
     strcpy(buf, g_buf);
 }
 
 void bof2() {
     char buf[0x10];
-    fread(g_buf, sizeof(g_buf), 1, stdin);
+    read(0, g_buf, sizeof(g_buf));
     size_t s_size = strlen(g_buf);
     strncpy(buf, g_buf, s_size);
 }
 
 int main(int argc, char **argv) {
-    // use it, don't optimize
-    if (argc == 1337) {
+    int num = 0;
+    if (argc >= 2) {
+        num = atoi(argv[1]);
+    }
+    if (num == 1337) {
         bof1();
-    } else if (argc == 1338) {
+    } else if (num == 1338) {
         bof2();
     }
     puts("hello world");
